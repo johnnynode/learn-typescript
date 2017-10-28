260689ff8f4be24f75d0c9b4baf60c817eb525b1
johnnynode <johnnynode@gmail.com>
2017-10-28 15:22:01 +0800

    type-annotation

---
+++ basic/3.type-annotation/README.md
@@ -0,0 +1,5 @@
+### 相关说明
+
+- 编译 $ `tsc greeter.ts`
+- 打开index.html 看到效果
+
---
+++ basic/3.type-annotation/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: number[]) {
+    return "Hello, " + person
+}
+
+let user = [0, 1, 2];
+
+document.body.innerHTML = greeter(user);
